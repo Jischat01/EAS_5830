2b372b19ddc48537d9c2d10308ddce7aa957258e
Jischat01 <jischat@seas.upenn.edu>
2025-07-12 15:23:10 -0400

    Add files via upload

--- signatures.py
+++ signatures.py
@@ -35,7 +35,7 @@ def verify(m, public_key, signed_message):
     # TODO verify the 'signed_message' is valid given the original message 'm' and the signers 'public_key'
     message = encode_defunct(text=m)  # Encode the message
     signer = eth_account.Account.recover_message(message, signature=signed_message.signature)  # Verify the message
-    valid_signature = signer  # True if message verifies, False if message does not verify
+    valid_signature = signer == public_key # True if message verifies, False if message does not verify
 
     assert isinstance(valid_signature, bool), "verify should return a boolean value"
     return valid_signature
