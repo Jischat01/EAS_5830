25b99eed681f2c782924e578144ea54b500f62fa
Jischat01 <jischat@seas.upenn.edu>
2025-08-02 17:19:14 -0400

    Add files via upload

--- listener.py
+++ listener.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import ExtraDataToPOAMiddleware  # Necessary for POA chains
+from web3.middleware import ExtraDataToPOAMiddleware #Necessary for POA chains
 from pathlib import Path
 import json
 from datetime import datetime
@@ -14,16 +14,16 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
     end_block - integer last block to scan
     contract_address - the address of the deployed contract
 
-    This function reads "Deposit" events from the specified contract,
-    and writes information about the events to the file "deposit_logs.csv"
+	This function reads "Deposit" events from the specified contract, 
+	and writes information about the events to the file "deposit_logs.csv"
     """
     if chain == 'avax':
-        api_url = f"https://api.avax-test.network/ext/bc/C/rpc"  # AVAX C-chain testnet
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
 
     if chain == 'bsc':
-        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/"  # BSC testnet
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
 
-    if chain in ['avax', 'bsc']:
+    if chain in ['avax','bsc']:
         w3 = Web3(Web3.HTTPProvider(api_url))
         # inject the poa compatibility middleware to the innermost layer
         w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
@@ -41,17 +41,19 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
         end_block = w3.eth.get_block_number()
 
     if end_block < start_block:
-        print(f"Error end_block < start_block!")
-        print(f"end_block = {end_block}")
-        print(f"start_block = {start_block}")
+        print( f"Error end_block < start_block!" )
+        print( f"end_block = {end_block}" )
+        print( f"start_block = {start_block}" )
 
     if start_block == end_block:
-        print(f"Scanning block {start_block} on {chain}")
+        print( f"Scanning block {start_block} on {chain}" )
     else:
-        print(f"Scanning blocks {start_block} - {end_block} on {chain}")
+        print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
 
     if end_block - start_block < 30:
-        events = contract.events.Deposit().get_logs(fromBlock=start_block, toBlock=end_block, argument_filters=arg_filter)
+        event_filter = contract.events.Deposit.create_filter(from_block=start_block,to_block=end_block,argument_filters=arg_filter)
+        events = event_filter.get_all_entries()
+        #print( f"Got {len(events)} entries for block {block_num}" )
         # TODO YOUR CODE HERE
         log_entries = []
         for evt in events:
@@ -70,8 +72,10 @@ def scan_blocks(chain, start_block, end_block, contract_address, eventfile='depo
             df.to_csv(eventfile, index=False)
 
     else:
-        for block_num in range(start_block, end_block + 1):
-            events = contract.events.Deposit().get_logs(fromBlock=block_num, toBlock=block_num, argument_filters=arg_filter)
+        for block_num in range(start_block,end_block+1):
+            event_filter = contract.events.Deposit.create_filter(from_block=block_num,to_block=block_num,argument_filters=arg_filter)
+            events = event_filter.get_all_entries()
+            #print( f"Got {len(events)} entries for block {block_num}" )
             # TODO YOUR CODE HERE
             log_entries = []
             for evt in events:
